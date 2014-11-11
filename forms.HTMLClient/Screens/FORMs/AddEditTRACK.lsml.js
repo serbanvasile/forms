798078dfba0775a269d4611ab8f9fd7c14ac1ecc
serbanvasile <serbanvasile@comcast.net>
2014-11-11 15:21:42 -0500

    Finally started the Track section. Got main track created, and then a partially working screens with form, field and choices (values).

---
+++ forms.HTMLClient/Screens/FORMs/AddEditTRACK.lsml.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
+
+myapp.AddEditTRACK.Method_execute = function (screen) {
+    // Write code here.
+    return myapp.applyChanges().then(function () {
+        var newTrackFormField = myapp.activeDataWorkspace.ApplicationData.TRACK_FORM_FIELDs.addNew();
+        newTrackFormField.setTRACK(screen.TRACK);
+        myapp.showAddEditTRACK_FORM_FIELD(newTrackFormField);
+    });
+};
+
+//myapp.AddEditTRACK.GetFormFieldLink_execute = function (screen) {
+//    // Write code here.
+//    // The hash is present on non-home screens.  Since we're
+//    // doing this on the home screen we could skip this part
+//    // but here it is for completeness.
+//    var hashPos = window.location.href.indexOf("#");
+//    if (hashPos >= 0)
+//        var baseUrl = window.location.href.substr(0, hashPos);
+//    else
+//        var baseUrl = window.location.href;
+//    //var fullUrl = baseUrl + "?entity=ApplicationData/TRACK_FORM_FIELDs(" +
+//    //              screen.TRACK_FORM_FIELDs.selectedItem.Id + ")";
+//    //screen.FormFieldUrl = fullUrl;
+//    //screen.showPopup("FormFieldPopUp");
+
+//};
+
---
+++ forms.Server/DataSources/ApplicationData/FORM_FIELD.lsml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.LightSwitch;
+namespace LightSwitchApplication
+{
+    public partial class FORM_FIELD
+    {
+        partial void FORM_FIELD_NAME_Compute(ref string result)
+        {
+            // Set result to the desired field value
+            result = this.FIELD.FIELD_LABEL;
+        }
+    }
+}
