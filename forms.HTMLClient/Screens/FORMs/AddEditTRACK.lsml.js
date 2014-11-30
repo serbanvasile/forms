aeede8f1ef67110f58df3f16e617ebc944ba7c5e
serbanvasile <serbanvasile@comcast.net>
2014-11-30 09:59:14 -0500

    Finally managed to replace old answers with new answers.

--- forms.HTMLClient/Screens/FORMs/AddEditTRACK.lsml.js
+++ forms.HTMLClient/Screens/FORMs/AddEditTRACK.lsml.js
@@ -1,13 +1,5 @@
 ﻿/// <reference path="~/GeneratedArtifacts/viewModel.js" />
 
-myapp.AddEditTRACK.Method_execute = function (screen) {
-    return myapp.applyChanges().then(function () {
-        var newTrackFormField = myapp.activeDataWorkspace.ApplicationData.TRACK_FORM_FIELDs.addNew();
-        newTrackFormField.setTRACK(screen.TRACK);
-        myapp.showAddEditTRACK_FORM_FIELD(newTrackFormField);
-    });
-};
-
 //myapp.AddEditTRACK.GetFormFieldLink_execute = function (screen) {
 //    // Write code here.
 //    // The hash is present on non-home screens.  Since we're
@@ -27,18 +19,17 @@ myapp.AddEditTRACK.Method_execute = function (screen) {
 
 
 myapp.AddEditTRACK.CHOICEs_BY_FORMFIELDID_Tap_execute = function (screen) {
-    var refTrackFormField = screen.FORM_FIELDs_BY_FORMID.selectedItem;
-    var existingTrackFormField = screen.TRACK_FORM_FIELDs.selectedItem;
-    var newTrackFormField;
-    if (existingTrackFormField) {
-        newTrackFormField = existingTrackFormField;
-    } else {
-        newTrackFormField = myapp.activeDataWorkspace.ApplicationData.TRACK_FORM_FIELDs.addNew();
-    }     
+
+    var newTrackFormField = myapp.activeDataWorkspace.ApplicationData.TRACK_FORM_FIELDs.addNew();
     newTrackFormField.setTRACK(screen.TRACK);
     newTrackFormField.setFORM_FIELD(screen.FORM_FIELDs_BY_FORMID.selectedItem);
     newTrackFormField.setVALUE(screen.CHOICEs_BY_FORMFIELDID.selectedItem);
 
+    if (screen.TRACK_FORM_FIELDs.selectedItem) {
+        screen.getTRACK_FORM_FIELDs().then(function (TRACK_FORM_FIELDs) {
+            TRACK_FORM_FIELDs.deleteSelected();
+        });
+    } 
     return myapp.applyChanges()
         .then(function () {
             screen.closePopup("popChoiceByFormFieldId");
@@ -65,10 +56,13 @@ myapp.AddEditTRACK.DeleteCurrentValue_Tap_execute = function (screen) {
     });
 };
 
-
-myapp.AddEditTRACK.VALUE_postRender = function (element, contentItem) {
+myapp.AddEditTRACK.VALUE_render = function (element, contentItem) {
     // Write code here.
+    var valueId = contentItem.data.VALUE.Id;
+    var fieldValue = $("<p>" + contentItem.data.VALUE.CHOICE_STR + "</p>"); 
     if (contentItem.data.FORM_FIELD.FIELD.FIELD_NAME != contentItem.parent.parent.data.FIELD.FIELD_NAME) {
         $(element).parentsUntil(".msls-listview").remove();
+    } else {
+        fieldValue.appendTo($(element));
     }
 };
