2d1f3c937855f141bf9da6c3671eb64d831c63c1
serbanvasile <serbanvasile@comcast.net>
2014-12-08 10:24:48 -0500

    Figured out how to bind actions against the selectedItem even of the visual collection.

--- forms.HTMLClient/Screens/FORMs/AddEditTRACK.lsml.js
+++ forms.HTMLClient/Screens/FORMs/AddEditTRACK.lsml.js
@@ -20,24 +20,26 @@
 
 myapp.AddEditTRACK.CHOICEs_BY_FORMFIELDID_Tap_execute = function (screen) {
 
+    //add new track form field
     var newTrackFormField = myapp.activeDataWorkspace.ApplicationData.TRACK_FORM_FIELDs.addNew();
     newTrackFormField.setTRACK(screen.TRACK);
-    newTrackFormField.setFORM_FIELD(screen.FORM_FIELDs_BY_FORMID.selectedItem);
     newTrackFormField.setVALUE(screen.CHOICEs_BY_FORMFIELDID.selectedItem);
+    newTrackFormField.setFORM_FIELD(screen.FORM_FIELDs_BY_FORMID.selectedItem);
 
+    //delete existing track form field
     if (screen.TRACK_FORM_FIELDs.selectedItem) {
-        screen.getTRACK_FORM_FIELDs().then(function (TRACK_FORM_FIELDs) {
-            TRACK_FORM_FIELDs.deleteSelected();
-        });
-    } 
+        screen.TRACK_FORM_FIELDs.deleteSelected();
+    }
+
     return myapp.applyChanges()
         .then(function () {
-            screen.closePopup("popChoiceByFormFieldId");
-        })
+                screen.closePopup("popChoiceByFormFieldId");
+            })
         .then(function () {
             screen.FORM_FIELDs_BY_FORMID.load(true);
-        });
- };
+        })
+};
+
 myapp.AddEditTRACK.CancelDeleteValue_Tap_execute = function (screen) {
     // Write code here.
     screen.closePopup("popDeleteValue");
@@ -65,4 +67,32 @@ myapp.AddEditTRACK.VALUE_render = function (element, contentItem) {
     } else {
         fieldValue.appendTo($(element));
     }
+};
+
+
+myapp.AddEditTRACK.created = function (screen) {
+
+    var formFieldsVisualCollection = screen.FORM_FIELDs_BY_FORMID;
+
+    function selectFirstField() {
+        screen.getTRACK_FORM_FIELDs().then(function (TRACK_FORM_FIELDs) {
+            $.each(TRACK_FORM_FIELDs.data, function (tff_key, tff_item) {
+                if (formFieldsVisualCollection.selectedItem &&
+                    formFieldsVisualCollection.selectedItem.Id == tff_item.FORM_FIELD.Id) {
+                        TRACK_FORM_FIELDs.selectedItem = tff_item;
+                        return false;
+                }
+            });
+        });
+    }
+
+    function onVisualCollectionSelectedItem() {
+        if (formFieldsVisualCollection.state === msls.VisualCollection.State.idle) {
+            //formFieldsVisualCollection.removeChangeListener("selectedItem", onVisualCollectionSelectedItem);
+            selectFirstField();
+        }
+    }
+
+    formFieldsVisualCollection.addChangeListener("selectedItem", onVisualCollectionSelectedItem);
+
 };
